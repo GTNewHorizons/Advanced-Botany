62cf59acbd64712dc55973e4b4b2d398a2ea5a6b
EnderProyects <enderproyects@gmail.com>
2023-04-04 20:39:46 +0200

    Error solved

--- src/main/java/ab/utils/ModHelperManager.java
+++ src/main/java/ab/utils/ModHelperManager.java
@@ -1,36 +1,27 @@
 package ab.common.utils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import ab.common.lib.register.RecipeListAB;
 
 public class ModHelperManager {
 
-    private static List<IModHelper> helpers;
+    private static IModHelper helper;
 
     public static void preInit() {
         setupHelpers();
 
-        for (IModHelper helper : helpers) {
-            helper.preInit();
-        }
+        helper.preInit();
     }
 
     public static void init() {
-        for (IModHelper helper : helpers) {
-            helper.init();
-        }
+        // helper.init();
     }
 
     public static void postInit() {
-        for (IModHelper helper : helpers) {
-            helper.postInit();
-        }
+        helper.postInit();
     }
 
     private static void setupHelpers() {
-        helpers = new ArrayList<>();
-        helpers.add(new RecipeListAB());
+
+        helper = new RecipeListAB();
     }
 }
